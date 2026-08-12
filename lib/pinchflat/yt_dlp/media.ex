68314eca785711e6ba825ff64937e76866c76fc1
Mitchell Abbott <profesorabbott@gmail.com>
2026-08-12 18:43:35 -0500

    update formatting

--- lib/pinchflat/yt_dlp/media.ex
+++ lib/pinchflat/yt_dlp/media.ex
@@ -88,7 +88,7 @@ defmodule Pinchflat.YtDlp.Media do
     output_template = indexing_output_template()
 
     with {:ok, output} <-
-          backend_runner().run(url, :get_media_attributes, all_command_opts, output_template, addl_opts),
+        backend_runner().run(url, :get_media_attributes, all_command_opts, output_template, addl_opts),
          {:ok, parsed_json} <- ResponseDecoder.decode(output, :get_media_attributes) do
       {:ok, response_to_struct(parsed_json)}
     end
